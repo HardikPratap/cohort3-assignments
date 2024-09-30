bbad8847b9ce1dc879e5d47a2840351ec97d3068
Hardik Pratap <hardikpratap188@gmail.com>
2024-09-30 14:43:58 +0530

    4-chainPromise Done

--- week-2/week-2-async-js/hard (promises)/4-promise-chain.js
+++ week-2/week-2-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,50 @@
  */
 
 function wait1(t) {
+    return new Promise(resolve=>{
+        setTimeout(resolve,t*1000);
+    });
 
 }
 
 function wait2(t) {
+    return new Promise(resolve=>{
+        setTimeout(resolve,t*1000);
+    });
 
 }
 
 function wait3(t) {
+    return new Promise(resolve=>{
+        setTimeout(resolve,t*1000);
+    });
 
 }
 
 function calculateTime(t1, t2, t3) {
+    const start=Date.now();
+    
+    
+    return cb(t1,t2,t3).then(()=>{
+        const end = Date.now() - start;
+        return end;
+
+    });
+
 
 }
+function cb(t1,t2,t3){
+    return wait1(t1)
+        .then(()=>{
+            return wait2(t2)
+                .then(()=>{
+                    return wait3(t3);
+
+            });
+        });
+    
+}
+
+
 
 module.exports = calculateTime;
