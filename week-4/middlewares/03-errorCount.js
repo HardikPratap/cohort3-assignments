cc946630343ad55d05d219a3f3344b7eac2e4ec2
Hardik Pratap <hardikpratap188@gmail.com>
2024-10-10 21:33:58 +0530

    03-errorCount.js

---
+++ week-4/middlewares/03-errorCount.js
@@ -0,0 +1,13 @@
+//Defining error middleware in nodejs
+//Alaways define them at th end of program
+
+
+
+// const express = require('express')
+// const app=express()
+
+// app.use(function(err,res,req,next){
+//     res.status(404).send({});
+
+// })
+
