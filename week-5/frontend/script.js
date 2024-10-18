cd2a86e7329caeca2766bfe3efde7a71edd70065
Hardik Pratap <hardikpratap188@gmail.com>
2024-10-18 17:42:51 +0530

    taskify frontend done

--- week-5/frontend/script.js
+++ week-5/frontend/script.js
@@ -1,1 +1,27 @@
 //  start from here
+function add(){
+
+    const input =document.getElementById("task").value
+    // const btn= document.getElementById("add-tasks")
+
+    const divEl =document.createElement("div")
+    const data = document.createElement("h1")
+    const dltBtn= document.createElement("button")
+
+    data.innerHTML =input
+    dltBtn.innerHTML="Delete"
+
+    divEl.setAttribute("style" ,"display : flex ; gap : 20px  ; justify-content:center")
+    dltBtn.setAttribute("style","padding:2px")
+    data.setAttribute("style","font-size: 30px")
+
+    divEl.appendChild(data)
+    divEl.appendChild(dltBtn)
+
+    document.getElementById("todo-container").appendChild(divEl)
+
+    dltBtn.addEventListener("click",()=>{
+        divEl.remove();
+    })
+
+}
